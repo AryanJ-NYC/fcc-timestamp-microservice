9dc4fb035f0dc6ae3c4a27b0663dc0858d122213
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2019-02-16 11:19:39 -0500

    Finish video 2

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const app = require('express')();
 
+app.get('/api/timestamp/:dateString?', (request, response) => {
+  const dateString = request.params.dateString;
+
+  let date;
+  // If the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp.
+  if (!dateString) {
+    date = new Date();
+  } else {
+    // non-empty dateString
+    // if datestring is integer, convert dateString to an integer
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+  // If the date string is invalid the api returns a JSON having the structure  {"error" : "Invalid Date" }.
+  if (date.toString() === 'Invalid Date') {
+    response.json({ error: date.toString() });
+  } else {
+    // If the date string is valid the api returns a JSON having the structure {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+    response.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
+
 app.get('/', (request, response) => {
   response.send('Timestamp Microservice works!');
 });
